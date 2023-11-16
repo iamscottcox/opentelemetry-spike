0e3c624e2475580c9974833682bd9602ab919ac1
Scott Cox <scottcox89@gmail.com>
2023-11-16 12:24:34 +0000

    initialise sdk

---
+++ instrumentation.ts
@@ -0,0 +1,21 @@
+import { Resource } from "@opentelemetry/resources";
+import {
+  ConsoleMetricExporter,
+  PeriodicExportingMetricReader,
+} from "@opentelemetry/sdk-metrics";
+import { NodeSDK } from "@opentelemetry/sdk-node";
+import { ConsoleSpanExporter } from "@opentelemetry/sdk-trace-node";
+import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
+
+const sdk = new NodeSDK({
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: "my-service-name",
+    [SemanticResourceAttributes.SERVICE_VERSION]: "0.0.1",
+  }),
+  traceExporter: new ConsoleSpanExporter(),
+  metricReader: new PeriodicExportingMetricReader({
+    exporter: new ConsoleMetricExporter(),
+  }),
+});
+
+sdk.start();
