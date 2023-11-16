d3633db450e750afa869c13c112bf02e88566da2
Scott Cox <scottcox89@gmail.com>
2023-11-16 12:59:01 +0000

    back to automatic instrumentations

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,17 +1,13 @@
-import { Resource } from "@opentelemetry/resources";
+import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
 import {
   ConsoleMetricExporter,
   PeriodicExportingMetricReader,
 } from "@opentelemetry/sdk-metrics";
 import { NodeSDK } from "@opentelemetry/sdk-node";
 import { ConsoleSpanExporter } from "@opentelemetry/sdk-trace-node";
-import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
 
 const sdk = new NodeSDK({
-  resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: "my-service-name",
-    [SemanticResourceAttributes.SERVICE_VERSION]: "0.0.1",
-  }),
+  instrumentations: [getNodeAutoInstrumentations()],
   traceExporter: new ConsoleSpanExporter(),
   metricReader: new PeriodicExportingMetricReader({
     exporter: new ConsoleMetricExporter(),
