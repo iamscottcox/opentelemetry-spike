3ee9d2f8f30c55ec0c20657212f5c102aa4c36b8
Scott Cox <scottcox89@gmail.com>
2023-11-16 12:35:29 +0000

    create independent spans

--- app.ts
+++ app.ts
@@ -4,6 +4,19 @@ import { rollTheDice } from "./dice";
 
 const tracer = trace.getTracer("dice-server", "0.1.0");
 
+const doWork = () => {
+  const span1 = tracer.startSpan("work-1");
+  console.log("span 1");
+  const span2 = tracer.startSpan("work-2");
+  console.log("span 2");
+  const span3 = tracer.startSpan("work-3");
+  console.log("span 3");
+
+  span1.end();
+  span2.end();
+  span3.end();
+};
+
 const PORT: number = parseInt(process.env.PORT || "8080");
 const app: Express = express();
 
@@ -16,6 +29,12 @@ app.get("/rolldice", (req, res) => {
     return;
   }
   res.send(JSON.stringify(rollTheDice(rolls, 1, 6)));
+});
+
+app.get("/do-work", (req, res) => {
+  doWork();
+
+  res.send("Check console for the work!");
 });
 
 app.listen(PORT, () => {
