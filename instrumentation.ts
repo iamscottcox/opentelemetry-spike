f4f9d19f3eb5b899f0114dcae8a1486cebf31655
Scott Cox <scottcox89@gmail.com>
2023-11-16 16:20:32 +0000

    zipkin

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,17 +1,19 @@
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
-import {
-  ConsoleMetricExporter,
-  PeriodicExportingMetricReader,
-} from "@opentelemetry/sdk-metrics";
+import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-proto";
+import { ZipkinExporter } from "@opentelemetry/exporter-zipkin";
+import { PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
 import { NodeSDK } from "@opentelemetry/sdk-node";
-import { ConsoleSpanExporter } from "@opentelemetry/sdk-trace-node";
 
 const sdk = new NodeSDK({
-  instrumentations: [getNodeAutoInstrumentations()],
-  traceExporter: new ConsoleSpanExporter(),
-  metricReader: new PeriodicExportingMetricReader({
-    exporter: new ConsoleMetricExporter(),
+  traceExporter: new ZipkinExporter({
+    serviceName: "opentelemetry-spike",
   }),
+  metricReader: new PeriodicExportingMetricReader({
+    exporter: new OTLPMetricExporter({
+      url: "http://localhost:4318/v1/metrics", // url is optional and can be omitted - default is http://localhost:4318/v1/metrics
+      headers: {}, // an optional object containing custom headers to be sent with each request
+    }),
+  }),
+  instrumentations: [getNodeAutoInstrumentations()],
 });
-
 sdk.start();
