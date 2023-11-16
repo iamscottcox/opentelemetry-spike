5d8f2a802c783c2ef52e54fe8be29efde8ec509f
Scott Cox <scottcox89@gmail.com>
2023-11-16 16:46:10 +0000

    prometheus with exporter plugin

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,19 +1,16 @@
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
-import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-proto";
+import { PrometheusExporter } from "@opentelemetry/exporter-prometheus";
 import { ZipkinExporter } from "@opentelemetry/exporter-zipkin";
-import { PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
 import { NodeSDK } from "@opentelemetry/sdk-node";
 
 const sdk = new NodeSDK({
   traceExporter: new ZipkinExporter({
     serviceName: "opentelemetry-spike",
   }),
-  metricReader: new PeriodicExportingMetricReader({
-    exporter: new OTLPMetricExporter({
-      url: "http://localhost:4318/v1/metrics", // url is optional and can be omitted - default is http://localhost:4318/v1/metrics
-      headers: {}, // an optional object containing custom headers to be sent with each request
-    }),
+  metricReader: new PrometheusExporter({
+    port: 9464, // optional - default is 9464
   }),
   instrumentations: [getNodeAutoInstrumentations()],
 });
+
 sdk.start();
