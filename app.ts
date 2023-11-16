debb03d84bbab3b0ccdb9a61c3aa8db3bf30f44d
Scott Cox <scottcox89@gmail.com>
2023-11-16 12:27:05 +0000

    acquiring a tracer

--- app.ts
+++ app.ts
@@ -1,19 +1,20 @@
+import { trace } from "@opentelemetry/api";
 import express, { Express } from "express";
 import { rollTheDice } from "./dice";
+
+const tracer = trace.getTracer("dice-server", "0.1.0");
 
 const PORT: number = parseInt(process.env.PORT || "8080");
 const app: Express = express();
 
 app.get("/rolldice", (req, res) => {
   const rolls = req.query.rolls ? parseInt(req.query.rolls.toString()) : NaN;
-
   if (isNaN(rolls)) {
     res
       .status(400)
       .send("Request parameter 'rolls' is missing or not a number.");
     return;
   }
-
   res.send(JSON.stringify(rollTheDice(rolls, 1, 6)));
 });
 
--- dice.ts
+++ dice.ts
@@ -1,14 +1,16 @@
 /*dice.ts*/
+import { trace } from "@opentelemetry/api";
+
+const tracer = trace.getTracer("dice-lib");
+
 function rollOnce(min: number, max: number) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
 export function rollTheDice(rolls: number, min: number, max: number) {
   const result: number[] = [];
-
   for (let i = 0; i < rolls; i++) {
     result.push(rollOnce(min, max));
   }
-
   return result;
 }
